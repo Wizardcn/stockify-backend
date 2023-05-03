77a0751c1cf5d5b172d7eed94a5ec65179a26fb2
Wizardcn <supawat125947@gmail.com>
2023-05-03 14:40:40 +0700

    add transaction route

--- api/transaction.api.go
+++ api/transaction.api.go
@@ -1,7 +1,25 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func SetupTransactionAPI(router *gin.Engine) {
+	transactionAPIv2 := router.Group("api/v2")
+	{
+		transactionAPIv2.GET("/transaction", getTransaction)
+		transactionAPIv2.POST("/transaction", createTransaction)
+	}
+}
+
+func getTransaction(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"result": "get transaction"})
+}
+
+func createTransaction(c *gin.Context) {
+
+	c.JSON(http.StatusCreated, gin.H{"result": "create transaction"})
 
 }
