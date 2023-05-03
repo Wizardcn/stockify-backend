372062fd6ecfa4da14fcfe9e39d54b9fe7eb6781
Wizardcn <supawat125947@gmail.com>
2023-05-03 18:01:54 +0700

    create register endpoint

--- api/authen.api.go
+++ api/authen.api.go
@@ -1,9 +1,13 @@
 package api
 
 import (
+	"net/http"
+	"stockify/db"
 	"stockify/model"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func SetupAuthenAPI(router *gin.Engine) {
@@ -22,7 +26,26 @@ func register(c *gin.Context) {
 
 	var user model.User
 	if c.ShouldBind(&user) == nil {
-		c.JSON(201, gin.H{"result": "registered", "data": user})
+		user.Password, _ = hashPassword(user.Password)
+		user.CreatedAt = time.Now()
+		if err := db.GetDB().Create(&user).Error; err != nil {
+			c.JSON(http.StatusOK, gin.H{"result": "nok", "error": err})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"result": "ok", "data": user})
+		}
+
+	} else {
+		c.JSON(http.StatusUnauthorized, gin.H{"msg": "unable to bind data"})
 	}
 
+}
+
+// func checkPasswordHash(password, hash string) bool {
+// 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+// 	return err == nil
+// }
+
+func hashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
 }
