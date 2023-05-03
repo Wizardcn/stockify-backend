9fa36d31d4317488571c47a14b6047b930efac97
Wizardcn <supawat125947@gmail.com>
2023-05-03 14:41:25 +0700

    uncomment transaction, product group

--- api/api.go
+++ api/api.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func SetupRouter(router *gin.Engine) {
 
 	SetupAuthenAPI(router)
-	// SetupProductAPI(router)
-	// SetupTransactionAPI(router)
+	SetupProductAPI(router)
+	SetupTransactionAPI(router)
 
 }
