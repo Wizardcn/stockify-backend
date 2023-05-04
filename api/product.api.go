8c005338988af9358d85bb80bf56f8981ac8da48
Wizardcn <supawat125947@gmail.com>
2023-05-04 16:42:04 +0700

    write createProduct func to upload file

--- api/product.api.go
+++ api/product.api.go
@@ -1,9 +1,13 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 
 	"stockify/interceptor"
+	"stockify/model"
 
 	"github.com/gin-gonic/gin"
 )
@@ -26,5 +30,16 @@ func getProduct(c *gin.Context) {
 }
 
 func createProduct(c *gin.Context) {
-	c.JSON(http.StatusCreated, gin.H{"result": "create product"})
+	product := model.Product{}
+	product.Name = c.PostForm("name")
+	product.Stock, _ = strconv.ParseInt(c.PostForm("stock"), 10, 64)
+	product.Price, _ = strconv.ParseFloat(c.PostForm("price"), 64)
+	image, _ := c.FormFile("image")
+	product.Image = image.Filename
+
+	workingDir, _ := os.Getwd()
+	filePath := fmt.Sprintf("%s/uploaded/images/%s", workingDir, image.Filename)
+	c.SaveUploadedFile(image, filePath)
+
+	c.JSON(http.StatusOK, gin.H{"result": product})
 }
