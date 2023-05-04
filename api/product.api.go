59e05b2050942adf5a0401d98cc69f130c6164ef
Wizardcn <supawat125947@gmail.com>
2023-05-04 12:35:38 +0700

    first interceptor

--- api/product.api.go
+++ api/product.api.go
@@ -6,11 +6,22 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func myInterceptor(c *gin.Context) {
+	token := c.Query("token")
+	if token == "1234" {
+		c.Next()
+	} else {
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "invalid token"})
+		c.Abort()
+	}
+
+}
+
 func SetupProductAPI(router *gin.Engine) {
 	productAPIv2 := router.Group("/api/v2")
 	{
-		productAPIv2.GET("/product", getProduct)
-		productAPIv2.POST("/product", createProduct)
+		productAPIv2.GET("/product", myInterceptor, getProduct)
+		productAPIv2.POST("/product", myInterceptor, createProduct)
 	}
 }
 
