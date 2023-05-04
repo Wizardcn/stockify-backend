3bf886ed7aa757c906939dd14647d3df3272d1ed
Wizardcn <supawat125947@gmail.com>
2023-05-04 11:42:28 +0700

    modify login func

--- api/authen.api.go
+++ api/authen.api.go
@@ -19,19 +19,37 @@ func SetupAuthenAPI(router *gin.Engine) {
 }
 
 func login(c *gin.Context) {
-	c.JSON(201, gin.H{"status": "logged in"})
+	var user model.User
+
+	if c.ShouldBind(&user) == nil {
+		var queryUser model.User
+		if err := db.GetDB().First(&queryUser, "username = ?", user.Username).Error; err != nil {
+			c.JSON(200, gin.H{"result": "nok", "error": err})
+		} else if !checkPasswordHash(user.Password, queryUser.Password) {
+			c.JSON(200, gin.H{"result": "nok", "error": "invalid password"})
+		} else {
+			// token := interceptor.JwtSign(queryUser)
+			c.JSON(200, gin.H{"result": "ok", "data": user})
+		}
+
+	} else {
+		c.JSON(401, gin.H{"status": "unable to bind data"})
+	}
 }
 
 func register(c *gin.Context) {
 
 	var user model.User
+
 	if c.ShouldBind(&user) == nil {
 		user.Password, _ = hashPassword(user.Password)
 		user.CreatedAt = time.Now()
 		if err := db.GetDB().Create(&user).Error; err != nil {
 			c.JSON(http.StatusOK, gin.H{"result": "nok", "error": err})
 		} else {
-			c.JSON(http.StatusOK, gin.H{"result": "ok", "data": user})
+			var queryUser model.User
+			db.GetDB().First(&queryUser, "username = ?", user.Username)
+			c.JSON(http.StatusOK, gin.H{"result": "ok", "data": queryUser})
 		}
 
 	} else {
@@ -40,10 +58,10 @@ func register(c *gin.Context) {
 
 }
 
-// func checkPasswordHash(password, hash string) bool {
-// 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-// 	return err == nil
-// }
+func checkPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
 
 func hashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
