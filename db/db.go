271a02d7c3b441a1f072a75d25122cd78384f3a6
Wizardcn <supawat125947@gmail.com>
2023-05-05 16:12:28 +0700

    modify setup db func to use env var

--- db/db.go
+++ db/db.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"fmt"
+	"os"
 	"stockify/model"
 
 	"gorm.io/driver/mysql"
@@ -16,7 +18,18 @@ func GetDB() *gorm.DB {
 
 // SetupDB - setup database for sharing to all api
 func SetupDB() {
-	dsn := "root:admin1234@tcp(127.0.0.1:3306)/stockify?charset=utf8mb4&parseTime=True&loc=Local"
+
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	dbUser := os.Getenv("DB_USERNAME")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser,
+		dbPass,
+		dbHost,
+		dbName,
+	)
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
